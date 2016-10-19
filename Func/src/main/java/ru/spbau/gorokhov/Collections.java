1ebfee26fa549ed051f0e0c0f531bf0734b869f2
wackloner <wackloner@gmail.com>
2016-10-19 14:32:10 +0300

    change Collections wildcards: extends -> super

--- Func/src/main/java/ru/spbau/gorokhov/Collections.java
+++ Func/src/main/java/ru/spbau/gorokhov/Collections.java
@@ -8,9 +8,7 @@ import java.util.List;
  * Created by wackloner on 15.10.2016.
  */
 public class Collections {
-    private Collections() {}
-
-    public static <A, V> List<V> map(Function1<A, V> f, Iterable<A> a) {
+    public static <A, V> List<V> map(Function1<? super A, V> f, Iterable<A> a) {
         List<V> res = new ArrayList<>();
         for (A x : a) {
             res.add(f.apply(x));
@@ -18,7 +16,7 @@ public class Collections {
         return res;
     }
 
-    public static <A> List<A> filter(Predicate<A> p, Iterable<? extends A> a) {
+    public static <A> List<A> filter(Predicate<? super A> p, Iterable<A> a) {
         List<A> res = new ArrayList<>();
         for (A x : a) {
             if (p.apply(x)) {
@@ -28,7 +26,7 @@ public class Collections {
         return res;
     }
 
-    public static <A> List<A> takeWhile(Predicate<A> p, Iterable<? extends A> a) {
+    public static <A> List<A> takeWhile(Predicate<? super A> p, Iterable<A> a) {
         List<A> res = new ArrayList<>();
         for (A x : a) {
             if (!p.apply(x)) {
@@ -39,11 +37,11 @@ public class Collections {
         return res;
     }
 
-    public static <A> List<A> takeUnless(Predicate<A> p, Iterable<? extends A> a) {
+    public static <A> List<A> takeUnless(Predicate<? super A> p, Iterable<A> a) {
         return takeWhile(p.not(), a);
     }
 
-    public static <A, V> V foldl(Function2<V, A, V> f, V startValue, Iterable<? extends A> a) {
+    public static <A, V> V foldl(Function2<V, ? super A, V> f, V startValue, Iterable<A> a) {
         V res = startValue;
         for (A x : a) {
             res = f.apply(res, x);
@@ -51,11 +49,11 @@ public class Collections {
         return res;
     }
 
-    public static <A, V> V foldr(Function2<V, A, V> f, V startValue, Iterable<? extends A> a) {
+    public static <A, V> V foldr(Function2<V, ? super A, V> f, V startValue, Iterable<A> a) {
         return foldr(f, startValue, a.iterator());
     }
 
-    private static <A, V> V foldr(Function2<V, A, V> f, V startValue, Iterator<? extends A> it) {
+    private static <A, V> V foldr(Function2<V, ? super A, V> f, V startValue, Iterator<A> it) {
         if (!it.hasNext()) {
             return startValue;
         }
