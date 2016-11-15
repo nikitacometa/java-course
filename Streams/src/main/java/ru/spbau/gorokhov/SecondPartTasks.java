0bba3b46c614aeb6ed3fb45462b6f609310aa18e
wackloner <wackloner@gmail.com>
2016-11-16 02:27:59 +0300

    add almost all SecondPart tasks

--- Streams/src/main/java/ru/spbau/gorokhov/SecondPartTasks.java
+++ Streams/src/main/java/ru/spbau/gorokhov/SecondPartTasks.java
@@ -1,7 +1,14 @@
 package ru.spbau.gorokhov;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * Created by wackloner on 14.11.2016.
@@ -12,7 +19,20 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths
+                .stream()
+                .flatMap(path -> {
+                    try {
+                        return Files.lines(Paths.get(path));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .filter(s -> s.contains(sequence))
+                .collect(
+                        Collectors.toList()
+                );
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -25,12 +45,30 @@ public final class SecondPartTasks {
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet()
+                .stream()
+                .max(
+                        Comparator.comparingInt(
+                                entry -> entry.getValue()
+                                .stream()
+                                .mapToInt(String::length)
+                                .sum()
+                        )
+                )
+                .get() //TODO: empty map handling
+                .getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders
+                .stream()
+                .flatMap(order -> order.entrySet().stream())
+                .collect(
+                        HashMap::new,
+                        (Map<String, Integer> map, Map.Entry<String, Integer> element) -> map.put(element.getKey(), map.getOrDefault(element.getKey(), 0) + element.getValue()),
+                        (map1, map2) -> map1.putAll(map2)
+                );
     }
 }
