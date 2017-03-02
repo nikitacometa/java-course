d421171c1522aa835cb6a064f37d215b57be5d82
wackloner <wackloner@gmail.com>
2017-03-02 19:59:04 +0300

    add javadoc

--- Lazy/src/main/java/ru/spbau/gorokhov/MultiThreadLockFreeLazy.java
+++ Lazy/src/main/java/ru/spbau/gorokhov/MultiThreadLockFreeLazy.java
@@ -8,6 +8,13 @@ import javax.xml.ws.Holder;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.Supplier;
 
+/**
+ * Implementation of Lazy interface that works correctly in multi thread
+ * mode without using locks. Can perform computation several times but returns
+ * the same resulting object for every get() call.
+ *
+ * @param <T> type of computation's result.
+ */
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public class MultiThreadLockFreeLazy<T> implements Lazy<T> {
     private static final AtomicReferenceFieldUpdater<MultiThreadLockFreeLazy, Holder> resultUpdater =
@@ -17,6 +24,13 @@ public class MultiThreadLockFreeLazy<T> implements Lazy<T> {
     private final Supplier<T> supplier;
     private volatile Holder<T> result;
 
+    /**
+     * Method which returns result of the computation. Computation can
+     * be performed several times by different threads, but result object
+     * is the same for all calls.
+     *
+     * @return result of computation.
+     */
     @Override
     public T get() {
         if (result == null) {
