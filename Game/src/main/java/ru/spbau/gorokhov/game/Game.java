9dc8c7e6d3de0ae6aeee2de62e83ce887a122105
wackloner <wackloner@gmail.com>
2017-05-11 16:22:07 +0300

    add asking size dialog

--- Game/src/main/java/ru/spbau/gorokhov/game/Game.java
+++ Game/src/main/java/ru/spbau/gorokhov/game/Game.java
@@ -7,35 +7,43 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
+/**
+ * Class implements the game where user has table NxN and
+ * wants to much every cell with number X with another cell
+ * with number X (there're two cells for each numbers in [0, N*N/2).
+ */
 public class Game extends Application {
-    private final static int SIZE = 2;
+    private final static int DEFAULT_SIZE = 2;
 
-    private final static long WAIT_TIME = 1000;
+    private final static long WAIT_TIME = 400;
 
-    private final static double CELL_SIZE = 40;
+    private final static double CELL_SIZE = 50;
 
 
     private Stage primaryStage;
 
-    private final int[][] numbers = new int[SIZE][SIZE];
+    private int[][] numbers;
 
-    private final Button[][] cells = new Button[SIZE][SIZE];
+    private Button[][] buttons;
 
-    private final Cell[][] positions = new Cell[SIZE][SIZE];
+    private Cell[][] cells;
 
-    private final List<Cell> aliveCells = new ArrayList<>();
+    private List<Cell> aliveCells;
 
     private long startTime;
 
+    @Nullable
     private Cell chosenCell = null;
 
     @Override
@@ -44,38 +52,70 @@ public class Game extends Application {
 
         primaryStage.setTitle("Game 1.0");
 
-        startNewGame();
+        showStartDialog("Let's play some!");
 
         primaryStage.show();
     }
 
-    private void startNewGame() {
-        primaryStage.setScene(new Scene(initTableCells()));
+    private void showStartDialog(String message) {
+        TextInputDialog dialog = new TextInputDialog(Integer.toString(DEFAULT_SIZE));
+        dialog.setTitle("New game");
+        dialog.setHeaderText(message);
+        dialog.setContentText("Enter size of field (positive even number):");
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            try {
+                int size = Integer.parseInt(result.get());
+
+                if (size <= 0 || size % 2 == 1) {
+                    showStartDialog("Number have to be positive and even!");
+                } else {
+                    startNewGame(size);
+                }
+            } catch (NumberFormatException e) {
+                showStartDialog("Invalid number format!");
+            }
+        } else {
+            Platform.exit();
+        }
+    }
+
+    private void startNewGame(int size) {
+        primaryStage.setScene(new Scene(initTableCells(size)));
 
         startTime = System.currentTimeMillis();
     }
 
     @NotNull
-    private GridPane initTableCells() {
-        initTableNumbers();
+    private GridPane initTableCells(int size) {
+        initTableNumbers(size);
+
+        aliveCells = new ArrayList<>();
+
+        cells = new Cell[size][size];
+
+        buttons = new Button[size][size];
 
         GridPane gridPane = new GridPane();
         gridPane.setGridLinesVisible(true);
 
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 int x = i, y = j;
 
-                cells[x][y] = new Button();
-                cells[x][y].setPrefSize(CELL_SIZE, CELL_SIZE);
+                buttons[x][y] = new Button();
+                buttons[x][y].setMaxSize(CELL_SIZE, CELL_SIZE);
+                buttons[x][y].setPrefSize(CELL_SIZE, CELL_SIZE);
+                buttons[x][y].setMinSize(CELL_SIZE, CELL_SIZE);
 
-                cells[x][y].setOnMouseClicked(event -> {
-                    cells[x][y].setText(Integer.toString(numbers[x][y]));
+                buttons[x][y].setOnMouseClicked(event -> {
+                    buttons[x][y].setText(Integer.toString(numbers[x][y]));
 
                     if (chosenCell == null) {
-                        chosenCell = positions[x][y];
+                        chosenCell = cells[x][y];
                     } else {
-                        aliveCells.forEach(pos -> cells[pos.getX()][pos.getY()].setDisable(true));
+                        aliveCells.forEach(cell -> buttons[cell.getX()][cell.getY()].setDisable(true));
 
                         boolean match = numbers[x][y] == numbers[chosenCell.getX()][chosenCell.getY()];
                         long waitTime = match ? 0 : WAIT_TIME;
@@ -93,18 +133,18 @@ public class Game extends Application {
                             @Override
                             protected void succeeded() {
                                 if (match) {
-                                    aliveCells.remove(positions[x][y]);
-                                    aliveCells.remove(positions[chosenCell.getX()][chosenCell.getY()]);
+                                    aliveCells.remove(cells[x][y]);
+                                    aliveCells.remove(cells[chosenCell.getX()][chosenCell.getY()]);
 
                                     if (aliveCells.size() == 0) {
                                         showWinAlert();
                                     }
                                 } else {
-                                    cells[x][y].setText("");
-                                    cells[chosenCell.getX()][chosenCell.getY()].setText("");
+                                    buttons[x][y].setText("");
+                                    buttons[chosenCell.getX()][chosenCell.getY()].setText("");
                                 }
 
-                                aliveCells.forEach(pos -> cells[pos.getX()][pos.getY()].setDisable(false));
+                                aliveCells.forEach(pos -> buttons[pos.getX()][pos.getY()].setDisable(false));
 
                                 chosenCell = null;
                             }
@@ -114,10 +154,10 @@ public class Game extends Application {
                     }
                 });
 
-                gridPane.add(cells[x][y], y, x);
+                gridPane.add(buttons[x][y], y, x);
 
-                positions[x][y] = new Cell(x, y);
-                aliveCells.add(positions[x][y]);
+                cells[x][y] = new Cell(x, y);
+                aliveCells.add(cells[x][y]);
             }
         }
         return gridPane;
@@ -131,22 +171,27 @@ public class Game extends Application {
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK){
-            startNewGame();
+            showStartDialog("Let's play again!");
         } else {
             Platform.exit();
         }
     }
 
-    private void initTableNumbers() {
+    private void initTableNumbers(int size) {
+        numbers = new int[size][size];
+
         List<Integer> gen = new ArrayList<>();
-        for (int i = 0; i < SIZE * SIZE / 2; i++) {
+
+        for (int i = 0; i < size * size / 2; i++) {
             gen.add(i);
             gen.add(i);
         }
+
         Collections.shuffle(gen);
+
         Iterator<Integer> iterator = gen.iterator();
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 numbers[i][j] = iterator.next();
             }
         }
