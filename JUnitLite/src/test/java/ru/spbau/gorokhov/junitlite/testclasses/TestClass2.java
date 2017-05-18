21b0c0c6298d4589983c4ef84770658b555a3897
wackloner <wackloner@gmail.com>
2017-05-18 17:10:10 +0300

    change exceptions being thrown in test classes so that travis could know them

--- JUnitLite/src/test/java/ru/spbau/gorokhov/junitlite/testclasses/TestClass2.java
+++ JUnitLite/src/test/java/ru/spbau/gorokhov/junitlite/testclasses/TestClass2.java
@@ -1,12 +1,11 @@
 package ru.spbau.gorokhov.junitlite.testclasses;
 
-import com.sun.javaws.exceptions.NativeLibViolationException;
 import ru.spbau.gorokhov.junitlite.annotations.Test;
 
 public class TestClass2 extends TestClass {
-    @Test(expected = NativeLibViolationException.class)
-    public void test1() throws NativeLibViolationException {
+    @Test(expected = NullPointerException.class)
+    public void test1() throws NullPointerException {
         counter++;
-        throw new NativeLibViolationException();
+        throw new NullPointerException();
     }
 }
--- JUnitLite/src/test/java/ru/spbau/gorokhov/junitlite/testclasses/TestClass3.java
+++ JUnitLite/src/test/java/ru/spbau/gorokhov/junitlite/testclasses/TestClass3.java
@@ -1,13 +1,13 @@
 package ru.spbau.gorokhov.junitlite.testclasses;
 
-import com.sun.javaws.exceptions.ErrorCodeResponseException;
-import com.sun.javaws.exceptions.NativeLibViolationException;
 import ru.spbau.gorokhov.junitlite.annotations.Test;
 
+import java.io.FileNotFoundException;
+
 public class TestClass3 extends TestClass {
-    @Test(expected = ErrorCodeResponseException.class)
-    public void test1() throws NativeLibViolationException {
+    @Test(expected = FileNotFoundException.class)
+    public void test1() throws NullPointerException {
         counter++;
-        throw new NativeLibViolationException();
+        throw new NullPointerException();
     }
 }
